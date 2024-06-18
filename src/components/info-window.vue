840f7b411231f219537c94c58491c280f8e4b314
AcaroMan <fabienfabio5@gmail.com>
2024-06-18 14:28:19 +0100

    Fix 1

--- src/components/info-window.vue
+++ src/components/info-window.vue
@@ -1,14 +1,17 @@
 /* vim: set softtabstop=2 shiftwidth=2 expandtab : */
 
 <template>
-  <div>
-    <div ref="flyaway"> <!-- so named because it will fly away to another component -->
-      <slot>
-      </slot>
+    <div>
+        <div ref="flyaway">
+            <!-- so named because it will fly away to another component -->
+            <slot> </slot>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default ((x) => x.default || x)(require('../components-implementation/info-window'))
+// export default ((x) => x.default || x)(require('../components-implementation/info-window'))
+//Use import instead of require
+import { InfoWindow } from "../components-implementation/info-window";
+export default ((x) => x.default || x)(InfoWindow);
 </script>
